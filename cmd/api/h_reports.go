0e5210a3da4aa4594f519232d5c44d30b807a538
Andres Hung <2018118240@ub.edu.bz>
2026-09-05 05:57:26 +0000

    chore: Clear QA comments

--- cmd/api/h_reports.go
+++ cmd/api/h_reports.go
@@ -26,9 +26,6 @@ func (app *application) createReportHandler(w http.ResponseWriter, r *http.Reque
 	}
 
 	// Validate the input fields for the job, returning a 422 response if any validation fails.
-	//
-	// NOTE: These validation checks can be abstracted into a ValidateJob method in the
-	// validator.Validator struct.
 	v := validator.New()
 	v.Check(input.ConsumerID != "", "consumer_id", "must be provided")
 	v.Check(!input.From.IsZero(), "from", "must be provided")
@@ -42,18 +39,6 @@ func (app *application) createReportHandler(w http.ResponseWriter, r *http.Reque
 	// Copy input fields to a job struct and insert it into the database, handling errors.
 	// A data.ErrRecordNotFound error can occur if the provided consumer_id does not exist in
 	// the database, as it is a foreign key constraint on the jobs table.
-	//
-	// NOTE: This error handling's form is different compared to that in createConsumerHandler,
-	// the latter which uses a switch construct. However, they are both functionally the same.
-	//
-	// Q01: A job is created instead of report generation happening here, which is the where
-	// the initial HTTP request is handled. If the report were generated here, it can cause problems
-	// with the HTTP request or response timing out, as it can potentially take a long time.
-	//
-	// Q02: This delegation of report generation to a job that is handled by a background worker
-	// satisfies the "Acknowledge without waiting for report completion" requirement, as the
-	// initial HTTP request is acknowledged immediately.
-
 	job := &data.Job{
 		ConsumerID: input.ConsumerID,
 		JobType:    "consumer_activity_report",
@@ -69,22 +54,11 @@ func (app *application) createReportHandler(w http.ResponseWriter, r *http.Reque
 	}
 
 	// Add a Location header to the response with the new job's public ID (UUIDv4).
-	//
-	// Q07: This is so that the client can locate the job later.
-	//
-	// Q08: The public ID is used instead of the internal database ID because UUIDv4 does not
-	// include a timestamp component, which could be a security risk as jobs are frequently polled.
 	statusURL := fmt.Sprintf("/v1/jobs/%s", job.PublicID)
 	headers := make(http.Header)
 	headers.Set("Location", statusURL)
 
 	// Send a JSON response of the newly created job, handling any errors.
-	//
-	// Q05: The HTTP 202 Accepted status code promises that the job has been accepted for processing,
-	// but does not guarantee that the job will be completed successfully.
-	//
-	// Q42: Since the reponse from the POST request is independent of the report generation,
-	// it can succeed even if the background job later fails.
 	response := envelope{"job_id": job.PublicID, "status": job.Status, "status_url": statusURL}
 	if err := app.writeJSON(w, http.StatusAccepted, response, headers); err != nil {
 		app.serverErrorResponse(w, r, err)
@@ -96,10 +70,6 @@ func (app *application) getJobHandler(w http.ResponseWriter, r *http.Request) {
 	// Retrieve the job from the database using the public ID from the URL, handling errors.
 	// Like createReportHandler, a data.ErrRecordNotFound error can occur if the provided
 	// job public ID does not exist in the database.
-	//
-	// NOTE: The github.com/julienschmidt/httprouter package is being used here to extract
-	// the public ID from the URL instead of the r.PathValue("id") in the starter code.
-	// The application architecture remains the same, only the implementation of the router is different.
 	job, err := app.models.Jobs.GetByPublicID((httprouter.ParamsFromContext(r.Context())).ByName("id"))
 	if err != nil {
 		if errors.Is(err, data.ErrRecordNotFound) {
