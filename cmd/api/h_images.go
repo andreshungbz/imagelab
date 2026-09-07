db0d1208fe13817d882e317d08f2fd7918a931a3
Andres Hung <2018118240@ub.edu.bz>
2026-09-07 15:59:06 -0600

    feat: Write image record and job record to the database and return appropriate response

--- cmd/api/h_images.go
+++ cmd/api/h_images.go
@@ -8,6 +8,7 @@ import (
 	_ "image/png"
 	"net/http"
 
+	"github.com/andreshungbz/imagelab/internal/data"
 	"github.com/andreshungbz/imagelab/internal/validator"
 )
 
@@ -23,7 +24,8 @@ func (app *application) processImageHandler(w http.ResponseWriter, r *http.Reque
 	}
 	defer file.Close()
 
-	// Image Validation
+	// IMAGE VALIDATION
+
 	v := validator.New()
 	// Validate image file and format.
 	_, format, err := image.DecodeConfig(file)
@@ -47,24 +49,65 @@ func (app *application) processImageHandler(w http.ResponseWriter, r *http.Reque
 	}
 
 	// Store the validated image using a server-controlled filename.
-	storedFilename, err := storeImage(file, "storage/uploads", format)
+	storedFilePath, err := storeImage(file, "storage/uploads", format)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
-	fmt.Println(storedFilename)
 
-	// TODO: Insert image record into the database to get the image ID.
+	// CREATE IMAGE RECORD
 
-	// TODO: Create a process_image_variants job in the database.
+	// Create a new Image instance with the gathered metadata.
+	mimeType := "image/jpeg"
+	if format == "png" {
+		mimeType = "image/png"
+	}
+	img := &data.Image{
+		OriginalFilename: header.Filename,
+		StoredFilename:   storedFilePath,
+		MIMEType:         mimeType,
+		SizeBytes:        header.Size,
+	}
+
+	// Insert the new image record into the database.
+	if err := app.models.Images.Insert(img); err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// CREATE IMAGE JOB (process_image_variants).
+
+	// Insert the new job record into the database.
+	job := &data.Job{
+		ConsumerID: app.config.consumerID, // Preconfigured ImageLab Consumer ID
+		JobType:    "process_image_variants",
+		Payload: data.ImagePayload{
+			ImageID:    img.ID,
+			SourcePath: img.StoredFilename,
+			Variants:   []string{"thumbnail", "preview", "display"},
+		},
+	}
+	if err := app.models.Jobs.Insert(job); err != nil {
+		if errors.Is(err, data.ErrRecordNotFound) {
+			app.notFoundResponse(w, r) // Provided Consumer ID not found.
+			return
+		}
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
 	// Add a Location header to the response with the new job's public ID (UUIDv4).
-	statusURL := fmt.Sprintf("/v1/jobs/%s", "placeholder-job-id")
+	statusURL := fmt.Sprintf("/v1/jobs/%s", job.PublicID)
 	headers := make(http.Header)
 	headers.Set("Location", statusURL)
 
 	// Send a JSON response of the newly created job, handling any errors.
-	response := envelope{"image_id": "placeholder-image-id", "job_id": "placeholder-job-id", "status": "queued", "status_url": "/v1/jobs/placeholder-job-id"}
+	response := envelope{
+		"image_id":   img.ID,
+		"job_id":     job.PublicID,
+		"status":     job.Status,
+		"status_url": statusURL,
+	}
 	if err := app.writeJSON(w, http.StatusAccepted, response, headers); err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
